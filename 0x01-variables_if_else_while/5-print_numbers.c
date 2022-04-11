aed7d06a0ae49baab29999a67a6cb19f1455126c
kalabmamo <kalabmamo5@gmail.com>
2022-04-11 18:42:32 +0300

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/* 
+ * auth: kalab mamo
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long number;
+
+	for (number = '0'; number <= 10; number++)
+			putchar(number);
+
+
+	putchar('\n');
+
+	return (0);
+}
